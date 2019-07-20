797e610d7b5470a93089ccd2e560d31dc6a32426
limbogPC <limbog.com@gmail.com>
2019-07-21 06:42:09 +0700

    create fun. preview img

--- lib/image_detail.dart
+++ lib/image_detail.dart
@@ -1,6 +1,14 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
 class ImageDetailPage extends StatefulWidget {
+
+  File imageFile;
+  ImageDetailPage({Key key, this.imageFile}) : super(key: key);
+
   @override
   _ImageDetailPageState createState() => _ImageDetailPageState();
 }
@@ -8,6 +16,8 @@ class ImageDetailPage extends StatefulWidget {
 class _ImageDetailPageState extends State<ImageDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Image.file(widget.imageFile)
+    );
   }
 }
