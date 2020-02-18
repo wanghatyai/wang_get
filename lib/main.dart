4b93aa50820f66d2ee61e3863ba0550d257cbf3e
wangAppPC <wanghatyai@gmail.com>
2020-02-18 17:09:48 +0700

    initial new wang get

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-import 'package:wang_get/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,16 +7,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ระบบรับสินค้า',
-      routes: <String,WidgetBuilder>{
-        '/Home': (BuildContext context) => Home(),
-      },
+      title: 'Flutter Demo',
       theme: ThemeData(
-
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'ระบบรับสินค้า'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -30,6 +28,14 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
@@ -38,167 +44,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-
-  TextEditingController ctrlUser = TextEditingController();
-  TextEditingController ctrlPass = TextEditingController();
-
-  var username;
-
-  var Useralert = 'Please enter the Username / กรุณากรอกชื่อบัญชีผู้';
-  var Passalert = 'Please enter the Password / กรุณากรอกรหัสผ่านบัญชีผู้ใช้';
-
-  var userInvalid = false;
-  var passInvalid = false;
-
-  void _showAlert() async {
-    return showDialog<void>(
-      context: context,
-      barrierDismissible: true, // user must tap button!
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('แจ้งเตือน'),
-          content: Text('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง'),
-        );
-      },
-    );
-  }
-
-  _checkPrefer() async{
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+  void _incrementCounter() {
     setState(() {
-      username = prefs.getString("empCodeReceive");
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
     });
-
-    if(username != null){
-      Navigator.pushReplacementNamed(context, '/Home');
-    }
   }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _checkPrefer();
-  }
-
-  _doLogin() async{
-
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    final response = await http.post(
-        'https://wangpharma.com/API/receiveProduct.php',
-        body: {'username': ctrlUser.text, 'password': ctrlPass.text, 'act': 'Login'});
-
-    if(ctrlUser.text != '' && ctrlPass.text != '' && response.statusCode == 200){
-
-      var jsonResponse = json.decode(response.body);
-
-      //print(jsonResponse);
-
-      if(!jsonResponse.isEmpty){
-
-        prefs.setString("empCodeReceive", ctrlUser.text);
-
-        Navigator.pushReplacementNamed(context, '/Home');
-      }else{
-        _showAlert();
-      }
-
-    }else{
-      _showAlert();
-    }
-  }
-
 
   @override
   Widget build(BuildContext context) {
-
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
       body: Center(
-
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'ระบบรับสินค้า',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              'You have pushed the button this many times:',
             ),
-            Padding (
-              padding: const EdgeInsets.all(20),
-              child: TextFormField (
-                controller: ctrlUser,
-                style: TextStyle (
-                  fontSize: 18,
-                  color: Colors.black,
-                ),
-                decoration: InputDecoration (
-                    prefixIcon: Icon (
-                      Icons.account_box,
-                      size: 30,
-                    ),
-                    labelText: 'Username / ชื่อบัญชีผู้ใช้',
-                    errorText: userInvalid ? Useralert : null,
-                    //'Please enter the Username / กรุณากรอกชื่อบัญชีผู้',
-                    labelStyle: TextStyle (
-                      color: Colors.blue,
-                      fontSize: (15),
-                    )
-                ),
-                keyboardType: TextInputType.number,
-              ),
-            ),
-            Padding (
-              padding: const EdgeInsets.all(20),
-              child: Stack (
-                alignment: AlignmentDirectional.centerEnd,
-                children: <Widget>[
-                  TextField (
-                    controller: ctrlPass,
-                    style: TextStyle (
-                      fontSize: 18,
-                      color: Colors.black,
-                    ),
-                    decoration: InputDecoration (
-                      prefixIcon: Icon (
-                        Icons.vpn_lock,
-                        size: 30,
-                      ),
-                      labelText: 'Password / รหัสผ่านบัญชีผู้ใช้',
-                      errorText: passInvalid ? Passalert : null,
-                      labelStyle: TextStyle (
-                        color: Colors.blue,
-                        fontSize: 15,
-                      ),
-                    ),
-                    keyboardType: TextInputType.number,
-                  ),
-                ],
-              ),
-            ),
-            Padding (
-              padding: const EdgeInsets.all(20),
-              child: SizedBox (
-                width: double.infinity,
-                height: 56,
-                child: RaisedButton (
-                  color: Colors.blue,
-                  onPressed: _doLogin,
-                  child: Text (
-                    'เข้าสู่ระบบ',
-                    style: TextStyle (
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
